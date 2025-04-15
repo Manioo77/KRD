a3fe89914382df892b77cff648c578602e73408d
Marek <mm729356@gmail.com>
2025-04-15 23:38:27 +0200

    updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,19 @@
-import react from "@vitejs/plugin-react";
-import path from "path";
-import { defineConfig } from "vitest/config";
+import react from '@vitejs/plugin-react'
+import path from 'path'
+import { defineConfig } from 'vitest/config'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
-  },
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "./setupTests.ts",
-  },
-});
+	plugins: [react()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, 'src'),
+		},
+	},
+	test: {
+		globals: true,
+		environment: 'jsdom',
+		setupFiles: './setupTests.ts',
+	},
+	base: '/KRD/',
+})
